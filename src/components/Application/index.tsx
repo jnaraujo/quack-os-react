14dbc4ce92fea14d68622404aa0e9b0ca0d12beb
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-10-23 19:46:58 -0300

    ✨ feat: add lazy load to apps

--- src/components/Application/index.tsx
+++ src/components/Application/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useMemo, useState } from "react";
+import { memo, Suspense, useEffect, useMemo, useState } from "react";
 import Draggable from "react-draggable";
 import { useWindowSize } from "react-use";
 import { useApps } from "../../hooks/useApp";
@@ -6,77 +6,79 @@ import Title from "../Title";
 import { Content } from "./styles";
 import { IApplicationProps } from "./types";
 
-function Application(props: IApplicationProps) {
-    const [isDraggable, setIsDraggable] = useState(false);
+function Application({ Node, ...props }: IApplicationProps) {
+  const [isDraggable, setIsDraggable] = useState(false);
 
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    const { removeApp } = useApps();
-    const { width, height } = useWindowSize();
+  const { removeApp } = useApps();
+  const { width, height } = useWindowSize();
 
-    const cardWidth = 350;
-    const cardHeight = 270;
+  const cardWidth = 350;
+  const cardHeight = 270;
 
-    const position = useMemo(() => {
-        if (props.x && props.y) {
-            return {
-                x: props.x,
-                y: props.y,
-            };
-        } else {
-            return {
-                x: (width - cardWidth) / 2,
-                y: (height - cardHeight) / 2,
-            };
-        }
-    }, [props.x, props.y, width, height]);
+  const position = useMemo(() => {
+    if (props.x && props.y) {
+      return {
+        x: props.x,
+        y: props.y,
+      };
+    } else {
+      return {
+        x: (width - cardWidth) / 2,
+        y: (height - cardHeight) / 2,
+      };
+    }
+  }, [props.x, props.y, width, height]);
 
-    const move = () => {
-        setIsDraggable(true);
-    };
-    const stop = () => {
-        setIsDraggable(false);
-    };
+  const move = () => {
+    setIsDraggable(true);
+  };
+  const stop = () => {
+    setIsDraggable(false);
+  };
 
-    const close = () => {
-        setTimeout(() => {
-            removeApp(props.id);
-        }, 300);
-    };
+  const close = () => {
+    setTimeout(() => {
+      removeApp(props.id);
+    }, 300);
+  };
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 500);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
-    return (
-        <Draggable
-            defaultPosition={{
-                x: position.x,
-                y: position.y,
-            }}
-            disabled={!isDraggable}
-        >
-            <Content>
-                <div className="title" onMouseOver={move} onMouseOut={stop}>
-                    <div></div>
-                    <Title className={`${loading === true ? "loading" : "loaded"}`}>
-                        {props.title}
-                    </Title>
-                    <div className="close" onClick={close}></div>
-                </div>
+  return (
+    <Draggable
+      defaultPosition={{
+        x: position.x,
+        y: position.y,
+      }}
+      disabled={!isDraggable}
+    >
+      <Content>
+        <div className="title" onMouseOver={move} onMouseOut={stop}>
+          <div></div>
+          <Title className={`${loading === true ? "loading" : "loaded"}`}>
+            {props.title}
+          </Title>
+          <div className="close" onClick={close}></div>
+        </div>
 
-                <div className="back">
-                    <div
-                        className={`application ${loading === true ? "loading" : "loaded"}`}
-                    >
-                        <props.node appId={props.id} />
-                    </div>
-                </div>
-            </Content>
-        </Draggable>
-    );
+        <div className="back">
+          <div
+            className={`application ${loading === true ? "loading" : "loaded"}`}
+          >
+            <Suspense fallback={<div>Loading...</div>}>
+              <Node appId={props.id} />
+            </Suspense>
+          </div>
+        </div>
+      </Content>
+    </Draggable>
+  );
 }
 
 export default memo(Application);
--- src/pages/Desktop/helper.ts
+++ src/pages/Desktop/helper.ts
@@ -1,50 +1,51 @@
-import Calculator from "../../components/Calculator";
-import Clock from "../../components/Clock";
-import Navigator from "../../components/Navigator";
-import Terminal from "../../components/Terminal";
+import React from "react";
+
+const Navigator = React.lazy(() => import("../../components/Navigator"));
+const Terminal = React.lazy(() => import("../../components/Terminal"));
+const Clock = React.lazy(() => import("../../components/Clock"));
+const Calculator = React.lazy(() => import("../../components/Calculator"));
 
 const AppsOnDesktop = [
-    {
-        title: "Clock",
-        id: "clock",
-        icon: "icons/clock/Clock_Face.svg",
-        defaultPosition: {
-            x: 20,
-            y: 20,
-        },
-        node: Clock,
+  {
+    title: "Clock",
+    id: "clock",
+    icon: "icons/clock/Clock_Face.svg",
+    defaultPosition: {
+      x: 20,
+      y: 20,
     },
-    {
-        title: "Terminal",
-        id: "terminal",
-        icon: "/icons/applications/Terminal.png",
-        defaultPosition: {
-            x: 20,
-            y: 20,
-        },
-        node: Terminal,
+    Node: Clock,
+  },
+  {
+    title: "Terminal",
+    id: "terminal",
+    icon: "/icons/applications/Terminal.png",
+    defaultPosition: {
+      x: 20,
+      y: 20,
     },
-    {
-        title: "Duck's Boat Navigator",
-        id: "navigator",
-        icon: "/icons/applications/Brosen_windrose.svg",
-        defaultPosition: {
-            x: 20,
-            y: 20,
-        },
-        node: Navigator,
+    Node: Terminal,
+  },
+  {
+    title: "Duck's Boat Navigator",
+    id: "navigator",
+    icon: "/icons/applications/Brosen_windrose.svg",
+    defaultPosition: {
+      x: 20,
+      y: 20,
     },
-    {
-        title: "Calculator",
-        id: "calculator",
-        icon: "/icons/applications/calculator.svg",
-        defaultPosition: {
-            x: 20,
-            y: 20,
-        },
-        node: Calculator,
+    Node: Navigator,
+  },
+  {
+    title: "Calculator",
+    id: "calculator",
+    icon: "/icons/applications/calculator.svg",
+    defaultPosition: {
+      x: 20,
+      y: 20,
     },
+    Node: Calculator,
+  },
 ];
 
 export { AppsOnDesktop };
-
