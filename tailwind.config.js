c3b6631900a331aa8e9f427080bdd8f5f13ad6fe
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-07-22 17:53:04 -0300

    ✨ feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+  },
+  plugins: [],
+}
+
