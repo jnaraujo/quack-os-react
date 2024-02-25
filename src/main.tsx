95c72d0a4231c5dde37deba82fe301a9259178b6
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-02-25 12:33:57 -0300

    🔥 chore: replace preact to react

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
-import { render } from "preact"
+import React from "react"
+import ReactDOM from "react-dom/client"
 import App from "./App"
 
 import "./index.css"
 
-render(<App />, document.getElementById("root")!)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,22 @@
 import { defineConfig } from "vite"
 import { visualizer } from "rollup-plugin-visualizer"
+import react from "@vitejs/plugin-react"
 import wasm from "vite-plugin-wasm"
-import prefresh from "@prefresh/vite"
-import preact from "@preact/preset-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    prefresh(),
     wasm(),
     visualizer({
       filename: "stats.html",
       gzipSize: true,
     }),
-    preact(),
+    react(),
   ],
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
-    },
-  },
-  resolve: {
-    alias: {
-      react: "preact/compat",
     },
   },
 })
