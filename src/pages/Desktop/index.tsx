ac30169eccb87b841cd78bcc319299f6b6dd2ca2
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-10-19 19:49:02 -0300

    feat: add flex attribute to window provider wrapper

--- src/pages/Desktop/index.tsx
+++ src/pages/Desktop/index.tsx
@@ -97,10 +97,10 @@ const Desktop = () => {
 
   return (
     <>
-      <main className="fixed h-screen w-screen overflow-hidden bg-blue-700 bg-duck bg-[length:200px_200px] bg-center bg-no-repeat">
+      <main className="fixed flex h-screen w-screen flex-col overflow-hidden bg-blue-700 bg-duck bg-[length:200px_200px] bg-center bg-no-repeat">
         <TopBar />
 
-        <div className="h-full w-full bg-dot-pattern bg-[length:50px] bg-repeat">
+        <div className="flex-1 bg-dot-pattern bg-[length:50px] bg-repeat">
           {apps.map((app) => (
             <Application key={app.id} {...app} />
           ))}
